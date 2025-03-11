f1c79cc15a79203b16eee3a154b33d31b7d1fef6
Avery <anortonsmith@germantownfriends.org>
2025-03-11 09:38:00 -0400

    bug fix

--- vm/Main.java
+++ vm/Main.java
@@ -3,7 +3,7 @@ import java.nio.file.*;
 
 class Main {
     public static void main(String[] args) throws Exception {
-        new VM().run("factorial/solution.txt");
+        new VM().run("insertion-sort/solution.txt");
     }
 }
 
@@ -150,7 +150,7 @@ class VM {
                 ip++;
                 return;
             case "store":
-                memory[args[1]] = args[0];
+                memory[args[0]] = args[1];
                 lastMem = args[0];
                 ip++;
                 return;
