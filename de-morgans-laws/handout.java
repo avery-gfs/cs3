cc48963846d54073c2b902fb6b0be386a84ca8f2
Avery <anortonsmith@germantownfriends.org>
2025-04-14 09:59:03 -0400

    boolean

---
+++ de-morgans-laws/handout.java
@@ -0,0 +1,35 @@
+// Tim is a hipster.
+// Tim will only watch movies that aren't new or aren't famous.
+//
+// Will Tim watch?
+//
+//              new   not new
+//            ┌─────┬─────────┐
+//     famous │ no  │   yes   │
+//            ├─────┼─────────┤
+// not famous │ yes │   yes   │
+//            └─────┴─────────┘
+//
+// Complete the below code to match Tim's criteria:
+
+boolean willTimWatch(boolean isNew, boolean isFamous) {
+	return ___________________________________________;
+}
+
+// Daniel likes fun movies.
+// Daniel will only watch movies that aren't sad and aren't long.
+//
+// Will Daniel watch?
+//
+//            sad   not sad
+//          ┌─────┬─────────┐
+//     long │ no  │   no    │
+//          ├─────┼─────────┤
+// not long │ no  │   yes   │
+//          └─────┴─────────┘
+//
+// Complete the below code to match Daniels's criteria:
+
+boolean willDanielWatch(boolean isSad, boolean isLong) {
+	return ___________________________________________;
+}
