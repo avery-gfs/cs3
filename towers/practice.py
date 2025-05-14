f9a58cb02c7d59f618cea1e573cefb934b683270
Avery <anortonsmith@germantownfriends.org>
2025-05-14 12:37:47 -0400

    more structure

--- towers/practice.py
+++ towers/practice.py
@@ -4,34 +4,39 @@ import time
 height = 6
 
 def showRow(radius, sym):
-  padding = " " * (height - radius)
-  width = radius * 2 + 1
-  return padding + sym * width + padding
+	padding = " " * (height - radius)
+	width = radius * 2 + 1
+	return padding + sym * width + padding
 
 def showRows(tower):
-  numBlanks = height - len(tower) + 1
-  blanks = [showRow(0, "░")] * numBlanks
-  discs = [showRow(row, "▇") for row in reversed(tower)]
-  return blanks + discs
+	numBlanks = height - len(tower) + 1
+	blanks = [showRow(0, "░")] * numBlanks
+	discs = [showRow(row, "▇") for row in reversed(tower)]
+	return blanks + discs
 
 def showTowers():
-  combined = [" "] * (height + 1)
+	combined = [" "] * (height + 1)
 
-  for tower in towers:
-    rows = showRows(tower)
+	for tower in towers:
+		rows = showRows(tower)
 
-    for (index, row) in enumerate(rows):
-      combined[index] += row + " "
+		for (index, row) in enumerate(rows):
+			combined[index] += row + " "
 
-  os.system("clear")
+	os.system("clear")
 
-  for row in combined:
-    print(row)
+	for row in combined:
+		print(row)
 
-  time.sleep(0.2)
+	time.sleep(0.2)
+
+def moveSingle(src, dest):
+	towers[dest].append(towers[src][-1])
+	towers[src].pop()
+	showTowers()
 
 def solve(src, dest, depth):
-  pass # Your code goes here!
+	pass # Your code goes here!
 
 tower = list(reversed(range(1, height + 1)))
 towers = [tower, [], []]
